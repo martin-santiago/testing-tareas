bbb1eaaf577872de538aa23812ccf1d8ff6fb498
Ronaldo Sosa <ronaldo.sosa@uc.cl>
2023-09-16 21:08:14 -0300

    Implementing frequency, execution time and callers

--- T2/Codigo Base T2/instrumentor/function_record.py
+++ T2/Codigo Base T2/instrumentor/function_record.py
@@ -1,6 +1,24 @@
 class FunctionRecord:
     def __init__(self, funName):
         self.functionName = funName
+        self.freq = 0
+        self.callers = []
+        self.start_times = []
+        self.end_times = []
+        self.times_execution = []
+        self.max_time_execution = 0
+        self.min_time_execution = 0
+        self.avg_time_execution = 0
 
     def print_report(self):
-        print("{:<30} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format(self.functionName, 0, 0, 0, 0, 0, "[]"))
+        print("{:<30} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format(self.functionName, str(self.freq), "{:.5f}".format(
+            self.avg_time_execution), "{:.5f}".format(self.max_time_execution), "{:.5f}".format(self.min_time_execution), 0, str(self.callers)))
+
+    def calculate_execution_times(self):
+        for i in range(len(self.start_times)):
+            self.times_execution.append(
+                self.end_times[i] - self.start_times[i])
+        self.max_time_execution = max(self.times_execution)
+        self.min_time_execution = min(self.times_execution)
+        self.avg_time_execution = sum(
+            self.times_execution)/len(self.times_execution)
--- T2/Codigo Base T2/instrumentor/profiler.py
+++ T2/Codigo Base T2/instrumentor/profiler.py
@@ -7,25 +7,36 @@ class Profiler(AbstractProfiler):
 
     def __init__(self):
         self.records = {}
-    
+        self.callers_stack = []
+
     # search a record by name
     def get_record(self, functionName):
         if functionName not in self.records:
             self.records[functionName] = FunctionRecord(functionName)
         return self.records[functionName]
 
-    # metodo se llama cada vez que se ejecuta una funcion    
+     # metodo se llama cada vez que se ejecuta una funcion
     def fun_call_start(self, functionName, args):
         record = self.get_record(functionName)
-       
+        record.start_times.append(time.time())
+        record.freq += 1
+
+        if self.callers_stack:
+            current_caller = self.callers_stack[-1]
+            if current_caller != functionName and current_caller not in record.callers:
+                record.callers.append(current_caller)
+
+        self.callers_stack.append(functionName)
+
     def fun_call_end(self, functionName, returnValue):
-        pass
-    
+        record = self.get_record(functionName)
+        record.end_times.append(time.time())
+        self.callers_stack.pop()  # Eliminamos la función actual de la pila.
+
     # print report
     def print_fun_report(self):
         print("{:<30} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format('fun', 'freq', 'avg', 'max', 'min',
                                                                         'cache', 'callers'))
         for record in self.records.values():
+            record.calculate_execution_times()
             record.print_report()
-
-
